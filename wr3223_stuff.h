f072791e83657a4b64ce8e8f3f0378816b8f46ef
schmurgel-tg <40195371+schmurgel-tg@users.noreply.github.com>
2023-02-01 21:17:06 +0100

    Delete wr3223_stuff.h

--- wr3223_stuff.h
+++
@@ -1,7 +0,0 @@
-#include "wr3223_commands.h"
-#include "wr3223_connector.h"
-#include "wr3223_sensorConnectors.h"
-#include "wr3223_relaisValueDecoder.h"
-#include "wr3223_statusValueHolder.h"
-#include "wr3223_errorValueDecoder.h"
-#include "wr3223_modeValueDecoder.h"
